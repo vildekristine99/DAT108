8c7e120777328a04d3d49512ee6b2cfe224bdae2
vildekristine99 <578051@stud.hvl.no>
2019-09-30 15:25:08 +0200

    message

---
+++ Oblig2/src/no/hvl/dat108/Vare.java
@@ -0,0 +1,15 @@
+package no.hvl.dat108;
+
+public class Vare {
+    
+    private String name;
+    
+    public Vare(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
